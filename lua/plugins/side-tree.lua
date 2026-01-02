abd41a09d73d496248dc571c6e15100e05df6acf
SCRIPTERBLOX <scriptoblox@gmail.com>
2026-01-02 08:11:41 +0000

    Removed useless buffer name at the bottom

--- lua/plugins/side-tree.lua
+++ lua/plugins/side-tree.lua
@@ -6,13 +6,35 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "NvimTree",
+      callback = function()
+        vim.opt_local.laststatus = 0
+      end,
+    })
+    
     require("nvim-tree").setup {
       view = {
         width = 30,
         side = "left",
       },
+      actions = {
+        open_file = {
+          quit_on_open = false,
+          resize_window = false,
+        },
+      },
       renderer = {
         group_empty = true,
+        indent_markers = {
+          enable = true,
+        },
+      },
+      modified = {
+        enable = true,
+      },
+      update_focused_file = {
+        enable = false,
       },
       filters = {
         dotfiles = false,
